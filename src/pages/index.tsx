daec5cf68ddb7f0b30c38405706ec25ba0853133
ahmadfahrezi <ahmadfahrezi8127@gmail.com>
2023-03-30 13:55:33 +0800

    add cat pics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
             <h1 className="bg-blue-400">Hello World</h1>
             <p>Halo halo bandung ibu kota periangan</p>
             <p>Universiti malaya kebangaan</p>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg" />
         </>
     );
 }
