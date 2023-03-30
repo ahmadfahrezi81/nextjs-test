5b14c632b53ba59050bd3767dae20959989e653b
HaritsDaffa99 <haritsdaffa99@gmail.com>
2023-03-30 13:48:14 +0800

    tambah paragraf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     return (
         <>
             <h1 className="bg-blue-400">Hello World</h1>
+            <p>Halo halo bandung ibu kota periangan</p>
         </>
     );
 }
